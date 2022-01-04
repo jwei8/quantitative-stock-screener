fd8b2424930a7a46d6084d372de6c5cb3708acb7
jwei8 <skybeach221b@gmail.com>
2022-01-03 19:48:16 -0800

    added all sticker option

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import os
 import pandas as pd
 import talib
+import csv
 
 from typing import Pattern
 from flask import Flask, render_template, request
@@ -13,17 +14,40 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     pattern = request.args.get('pattern', None)
+    stocks = {}
+    results = {}
+
+    with open('datasets/symbols.csv') as f :
+         for row in csv.reader(f):
+             stocks[row[0]] = {'company' : row[1]}
+
     if pattern:
         datafiles = os.listdir('datasets/daily')
         for filename in datafiles:
             df = pd.read_csv('datasets/daily/{}'.format(filename))
-            pattern_function = getattr(talib, pattern)
+                
+
             try:
-                results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                if pattern == 'ALL TICKER' :
+                    symbol = filename.split('.')[0]
+                    stocks[symbol][pattern] = 'Neutral'
+                    
+                else :
+                    pattern_function = getattr(talib, pattern)
+                    symbol = filename.split('.')[0]
+                    results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                    last = results.tail(1).values[0]
                 print(results)
+                #shows which ticker triggered the pattern
+                if last > 0 :
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
             except Exception as e:
-                print('failed on filename: ', filename)
-    return render_template('index.html', candlestick_patterns = candlestick_patterns)
+                pass
+    return render_template('index.html', candlestick_patterns = candlestick_patterns, stocks=stocks, pattern=pattern)
 
 @app.route('/snapshot')
 def snapshot():
--- patterns.py
+++ patterns.py
@@ -59,5 +59,6 @@ candlestick_patterns = {
     'CDLTRISTAR':'Tristar Pattern',
     'CDLUNIQUE3RIVER':'Unique 3 River',
     'CDLUPSIDEGAP2CROWS':'Upside Gap Two Crows',
-    'CDLXSIDEGAP3METHODS':'Upside/Downside Gap Three Methods'
+    'CDLXSIDEGAP3METHODS':'Upside/Downside Gap Three Methods',
+    'ALL TICKER' : 'All'
 }
